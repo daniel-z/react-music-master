2dbaa9f1974557911aa719fdf8aee3fe5642d587
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2017-03-28 21:34:35 -0600

    Searching in spotify,

--- src/App.js
+++ src/App.js
@@ -11,7 +11,14 @@ class App extends Component {
   }
 
   search () {
-    console.log('this.query', this.state)
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));
+
   }
 
   render() {
@@ -23,7 +30,9 @@ class App extends Component {
             <FormControl
               type="text"
               placeholder="Search for an Artist"
-              onChange={event => {this.setState({query: event.target.value})}}
+              onChange={event => {
+                this.setState({query: event.target.value.replace(' ', '+')})
+              }}
               onKeyPress={event => {
                 if (event.key === 'Enter') {
                   this.search();
