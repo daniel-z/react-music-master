893049ce73abde3a297bcf6de66c8d3fdb7c2db0
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2017-03-28 21:15:37 -0600

    Adding basic styles.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search () {
+    console.log('this.query', this.state)
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app-title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+          <InputGroup.Addon>
+            <Glyphicon glyph="search"></Glyphicon>
+          </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <div className="profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          Gallery
+        </div>
       </div>
     );
   }
